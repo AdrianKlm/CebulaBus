653f0a9f1bfb0424ae8eec7266cb734d05ff18a8
Adrian <klimczykadrian@icloud.com>
2021-09-11 20:04:44 +0200

    cd

--- cebulabus.py
+++ cebulabus.py
@@ -12,13 +12,13 @@ cfg = configparser.ConfigParser()
 cfg.read('config.ini')
 flexbus_last_max_date = None
 neobus_last_max_date = None
-f = open('emailList.json',)
-log = open("cebulabus.log")
+f = open('emailList.json')
+log = open("cebulabus.log","a")
 emails = json.load(f)
 
 def write_log(text):
     print(text)
-    log.write(text)
+    log.write(text+'\n')
 
 def send_email(busname, newDate):
     msg = EmailMessage()
